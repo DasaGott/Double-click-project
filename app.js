1082e8802297b23d1beeaca641d95d344b243884
DasaGott <142871686+DasaGott@users.noreply.github.com>
2023-12-28 20:29:31 +0100

    Added moving effect to the text above the picture

--- app.js
+++ app.js
@@ -44,3 +44,29 @@ const createHeart = (e) => {
     heart.remove();
   }, 1000);
 };
+
+//Animation of moving text
+const textElement =
+  document.getElementById("text");
+const speedElement =
+  document.getElementById("speed");
+const text = `DoubleClick to add heart!`;
+
+let idx = 1;
+let speed = 300 / speedElement.value;
+
+writeText();
+function writeText() {
+  textElement.innerText = text.slice(0, idx);
+  idx++;
+
+  if (idx > text.length) {
+    idx = 1;
+  }
+
+  setTimeout(writeText, speed);
+}
+
+speedElement.addEventListener("input", (e) => {
+  speed = 300 / e.target.value;
+});
